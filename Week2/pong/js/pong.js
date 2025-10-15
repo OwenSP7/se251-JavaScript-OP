69587f8502dc656cede00d386beb8266c641aa43
OwenSP7 <ospimentel@email.neit.edu>
2025-10-14 22:54:26 -0400

    week 2 progress

--- Week2/pong/js/pong.js
+++ Week2/pong/js/pong.js
@@ -16,17 +16,22 @@ player2 = new Player
 player2.pad = new Box
 player.push(player1,player2)
 
+//Pad Array set up
+const pad = []
+pad[0] = player[0].pad
+pad[1] = player[1].pad
+
 //p1 setup
 var p1 = new Box();
-p1.w = 20
-p1.h = 150
-p1.x = 0 + p1.w/2
+pad[0].w = 20
+pad[0].h = 150
+pad[0].x = 0 + pad[0].w/2
 
 //p2 setup
 var p2 = new Box();
-p2.w = 20
-p2.h = 150
-p2.x = 800 - p2.w/2
+pad[1].w = 20
+pad[1].h = 150
+pad[1].x = 800 - pad[1].w/2
 
 //ball setup
 var ball = new Box();
@@ -44,52 +49,52 @@ function main()
     //p1 accelerates when key is pressed 
     if(keys[`w`])
     {
-       p1.vy += -p1.force
+       pad[0].vy += -pad[0].force
     }
 
     if(keys[`s`])
     {
-        p1.vy += p1.force
+        pad[0].vy += pad[0].force
     }
 
     //p2 accelerates when key is pressed 
     if(keys[`ArrowUp`])
     {
-       p2.vy += -p2.force
+       pad[1].vy += -pad[1].force
     }
 
     if(keys[`ArrowDown`])
     {
-        p2.vy += p2.force
+        pad[1].vy += pad[1].force
     }
     //applies friction
-    p1.vy *= fy
-    p2.vy *= fy
+    pad[0].vy *= fy
+    pad[1].vy *= fy
     //player movement
-    p1.move();
-    p2.move();
+    pad[0].move();
+    pad[1].move();
 
     //ball movement
     ball.move()
 
     //p1 collision
-    if(p1.y < 0+p1.h/2)
+    if(pad[0].y < 0+pad[0].h/2)
     {
-        p1.y = 0+p1.h/2
+        pad[0].y = 0+pad[0].h/2
     }
-    if(p1.y > c.height-p1.h/2)
+    if(pad[0].y > c.height-pad[0].h/2)
     {
-        p1.y = c.height-p1.h/2
-    }
-
-    if(p2.y < 0+p2.h/2)
-    {
-        p2.y = 0+p2.h/2
+        pad[0].y = c.height-pad[0].h/2
     }
 
-    if(p2.y > c.height-p2.h/2)
+    if(pad[1].y < 0+pad[1].h/2)
     {
-        p2.y = c.height-p2.h/2
+        pad[1].y = 0+pad[1].h/2
+    }
+
+    if(pad[1].y > c.height-pad[1].h/2)
+    {
+        pad[1].y = c.height-pad[1].h/2
     }
 
     //ball collision 
@@ -124,21 +129,21 @@ function main()
     }
 
     //p1 with ball collision
-    if(ball.collide(p1))
+    if(ball.collide(pad[0]))
     {
-        ball.x = p1.x + p1.w/2 + ball.w/2
+        ball.x = pad[0].x + pad[0].w/2 + ball.w/2
         ball.vx = -ball.vx;
     }
 
     //p2 with ball collision
-    if(ball.collide(p2))
+    if(ball.collide(pad[1]))
     {
-        ball.x = p2.x - p2.w/2 - ball.w/2
+        ball.x = pad[1].x - pad[1].w/2 - ball.w/2
         ball.vx = -ball.vx;
     }
 
     //draw the objects
-    p1.draw()
-    p2.draw()
+    pad[0].draw()
+    pad[1].draw()
     ball.draw()
 }
